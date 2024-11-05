fb9084bdbe4eb1aff8ee52b4a7c1cb1afaa8c868
gaojiaxiang <gaojiaxiang2018@gmail.com>
2024-11-05 14:41:22 +0800

    feat: feat2

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 export default () => {
-    const [count, setCount] = useState(0)
-    function a() {
-        console.log('111')
-        setCount(count + 1)
-    }
-    return (
-        <button >
-            {count}
-            <p onClick={a}>a</p>
-        </button>
-    )
-}
+  const [count, setCount] = useState(0);
+  function a() {
+    setCount(count + 1);
+  }
+  return (
+    <button>
+      {count}
+      <p onClick={a}>a</p>
+    </button>
+  );
+};
